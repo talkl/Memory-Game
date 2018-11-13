09bac7b632b9d30cfb11c7a0b9d105777b5eb86f
Tal_Kleiner <talkl120@gmail.com>
2018-11-13 19:17:28 +0200

    first commit

---
+++ js/memory_game.js
@@ -0,0 +1,25 @@
+/*Assignment #5 - Memory Game
+______________________________________________________________________________________
+The following exercise contains the following subjects:
+o JavaScript: Events handling, DOM Manipulation
+o HTML / CSS, Bootstrap
+
+Understanding the task:
+1. The board size should be 3 * 4; The cards should be dealt in random, every card has a match.
+2. When the user wins, pop up a “You won!” overlay with a new game button.
+3. Create a header with a “new game” button
+4. The game page has to be mobile responsive
+Important features:
+● After flipping two cards with different images, the game should pause for a second.During that
+second, the other cards are not clickable.
+● Don’t wait to finish your project before you commit your code to GitHub.Commit whenever you added
+an important functionality.
+● Make sure your JS code is using functions and well commented.
+Geek out:
+1. Add number of wrong guesses counter(put it anywhere).The “you won” overlay should show them.
+2. Add the ability to change the game theme(both images and card pattern).
+3. Make the basic layout the “easy” level, add levels medium and hard(with more 18 and 24 cards).
+Unleash the ninja within:
+1. Add flipping animation effect for the card.
+2. Add a high score functionality, that will save the name of the person with the least amounts of wrong
+guesses.*/
